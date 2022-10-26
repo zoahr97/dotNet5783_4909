9ef29e32326d4f2a78a23c8982b4e40e3a532c85
אסתר אירם <Schlessi@g.jct.ac.il>
2022-10-26 16:10:50 +0300

    I forgeted ;

---
+++ Stage0/Program3248.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Stage0
+{
+    partial class Program
+    {
+
+        static partial void welcome3248()
+        {
+            string str = Console.ReadLine();
+            Console.WriteLine("I am also here!" + str);
+        }
+    }
+
+}
