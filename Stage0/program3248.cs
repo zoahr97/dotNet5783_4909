9d08e33fa6598830b3702b1657e9d0733be814f4
אסתר אירם <Schlessi@g.jct.ac.il>
2022-10-26 16:07:22 +0300

    a new class file with a new class.

---
+++ Stage0/program3248.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main()
+        {
+            welcome3248();
+        }
+        static partial void welcome3248()
+        {
+            string str = Console.ReadLine()    
+            Console.WriteLine("I am also here!" + str);
+        }
+    }
+
+}
