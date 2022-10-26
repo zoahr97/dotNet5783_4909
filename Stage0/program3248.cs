4c638bf397517c38f724e9b097dbb42f82fd7ba4
אסתר אירם <Schlessi@g.jct.ac.il>
2022-10-26 16:08:14 +0300

    another class file with a new method to the class program.

--- Stage0/program3248.cs
+++ Stage0/program3248.cs
@@ -5,13 +5,10 @@ namespace Stage0
 {
     partial class Program
     {
-        static void Main()
-        {
-            welcome3248();
-        }
+
         static partial void welcome3248()
         {
-            string str = Console.ReadLine()    
+            string str = Console.ReadLine()
             Console.WriteLine("I am also here!" + str);
         }
     }
