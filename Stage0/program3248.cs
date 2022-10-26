eebe88b5e280f6dcad9f39ac6a6b1bd2fecfd558
zoahr97 <zoahr97@gmail.com>
2022-10-26 17:36:39 +0300

    more changes

--- Stage0/program3248.cs
+++ Stage0/program3248.cs
@@ -6,7 +6,7 @@ namespace Stage0
     partial class Program
     {
          
-        static partial void Welcome3248()
+        static  void Welcome3248()
         {
             
             Console.WriteLine("I am also here!");
