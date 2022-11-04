3762d5799515e8ccd9e337ea09d9264cac11bffc
אסתר אירם <Schlessi@g.jct.ac.il>
2022-11-04 14:31:42 +0200

    השינויים שביצעתי בינתיים...

--- dotNet5783_4909_3248/DalTest/Program.cs
+++ dotNet5783_4909_3248/DalTest/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace DalTest
+{
+    static void Main(string[] args)
+    {
+        private DalProduct myProduct = new DalProduct() ;
+        private DalOrder myOrder = new DalOrder() ;
+        private DalOrderItem myOrderItem = new DalOrderItem() ;
+    }
+}
